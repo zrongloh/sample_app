fe0ecea5f17011b94f056cf2e140d4b5abb4594f
zrongloh <zrongloh@gmail.com>
2014-06-26 20:05:33 +0800

    Add a full_title helper

--- Gemfile
+++ Gemfile
@@ -6,11 +6,17 @@ gem 'rails', '4.0.5'
 group :development, :test do
   gem 'sqlite3', '1.3.8'
   gem 'rspec-rails', '2.13.1'
+  gem 'guard-rspec', '2.5.0'
+  gem 'spork-rails', '4.0.0'
+  gem 'guard-spork', '1.5.0'
+  gem 'childprocess', '0.3.6'
 end
 
 group :test do
   gem 'selenium-webdriver', '2.35.1'
   gem 'capybara', '2.1.0'
+  gem 'rb-notifu', '0.0.4'
+  gem 'wdm','0.1.0'
 end
 
 gem 'sass-rails', '4.0.1'
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -9,9 +9,15 @@ describe "StaticPages" do
       expect(page).to have_content('Sample App')
     end
 
-    it "should have the right title" do
+    it "should have the base title" do
+      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
       visit '/static_pages/home'
-      expect(page).to have_title("#{base_title} | Home")
+      expect(page).to have_title("#{base_title}")
+    end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      expect(page).not_to have_title('| Home')
     end
   end
 
