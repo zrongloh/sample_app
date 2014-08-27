06b83d5564d24094273f3a3017f05706781b7947
zrongloh <zrongloh@gmail.com>
2014-08-27 16:27:27 +0800

    Add user following

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-before_action :signed_in_user,  only:[:index, :edit, :update, :destroy]
+before_action :signed_in_user,  only:[:index, :edit, :update, :destroy, :following, :followers]
 before_action :correct_user,    only:[:edit, :update]
 before_action :admin_user,      only: :destroy
 
@@ -46,19 +46,26 @@ before_action :admin_user,      only: :destroy
     end
   end
 
+  def following
+    @title = "Following"
+    @user=User.find(params[:id])
+    @users=@user.followed_users.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
+  def followers
+    @title="Followers"
+    @user=User.find(params[:id])
+    @users=@user.followers.paginate(page: params[:page])
+    render 'show_follow'
+  end
+  
   private
   	def user_params
   		params.require(:user).permit(:name, :email, :password, :password_confirmation)
   	end
 
   # Before filters
-
-  def signed_in_user
-    unless signed_in?
-      store_location
-      redirect_to signin_url, notice: "Please sign in." unless signed_in?
-    end
-  end
 
   def correct_user
     @user=User.find(params[:id])
